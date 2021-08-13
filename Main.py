a59d609eaf6a5ba609853c4d0ccccebf75cf7147
Parrishbritt1 <parrishbritt1@gmail.com>
2021-08-12 21:36:33 -0400

    Now moves into DUO authentication iframe and allows for input into text box (Still not detecting if you enter the wrong one in)

--- Main.py
+++ Main.py
@@ -4,8 +4,10 @@
 # Why: It's annoying to fill out the same answers every day and saving a whopping valuable 3 mins of my day lol
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from config import DRIVER_PATH, USERNAME, PASSWORD
+import time
 
 
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
@@ -13,27 +15,64 @@ driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 driver.get("https://itsapps2.gmu.edu/symptom/Account/Login/")
 
 # Click 'LOGIN WITH MASON NETID' button
-login_with_NETID_element = driver.find_element_by_id("gmulogin")
-login_with_NETID_element.click()
+login_with_NETID_ele = driver.find_element_by_id("gmulogin")
+login_with_NETID_ele.click()
 
 # Click field and enter username
-username_element = driver.find_element_by_id("username")
-username_element.click()
-username_element.send_keys(USERNAME)
+username_ele = driver.find_element_by_id("username")
+username_ele.click()
+username_ele.send_keys(USERNAME)
 
 # Click field and enter password
-password_element = driver.find_element_by_id("password")
-password_element.click()
-password_element.send_keys(PASSWORD)
+password_ele = driver.find_element_by_id("password")
+password_ele.click()
+password_ele.send_keys(PASSWORD)
 
 # Click login
-login_element = driver.find_element_by_name("_eventId_proceed")
-login_element.click()
+login_ele = driver.find_element_by_name("_eventId_proceed")
+login_ele.click()
 
-# Getting duo auth iframe
-duo_iframe = driver.find_element_by_id("duo_iframe")
-print(duo_iframe)
+# -------------------------------DUO Authentication---------------------------------
+frame = driver.find_element_by_id("duo_iframe")
+driver.switch_to.frame(frame)
 
+# Click remember for 14 days if not already selected
+remember_14_days_ele = driver.find_element_by_xpath("/html/body/div/div/div[1]/div/form/div[2]/div/label/input")
+if not remember_14_days_ele.is_selected():
+    remember_14_days_ele.click()
+
+# click passcode button
+passcode_button_ele = driver.find_element_by_id("passcode")
+passcode_button_ele.click()
+
+# Enter 2SA passcode
+while True:
+    time.sleep(.5)
+    duo_auth_passcode = input("Enter DUO Passcode: ")
+    passcode_text_field_ele = driver.find_element_by_class_name("passcode-input")
+    passcode_text_field_ele.click()
+    passcode_text_field_ele.send_keys(duo_auth_passcode)
+
+    login_button_ele = driver.find_element_by_id("passcode")
+    login_button_ele.click()
+    
+    # Making sure correct passcode is entered
+    try:
+        driver.find_element_by_xpath('/html/body/div/div/div[4]/div/div[2]')
+        
+        # Untested - may print when not supposed to
+        print("**Wrong Passcode**")
+        print("May have to refresh code on DUO app")
+    except NoSuchElementException:
+        print("Couldn't find error")
+        break
+
+# Leave iframe
+driver.switch_to.default_content()
+
+# Start Health Risk Assessment button
+# start_assessment_ele = driver.find_element_by_class_name("btn btn-yellow")
+# start_assessment_ele.click()
 
 
 # Closes the browser
